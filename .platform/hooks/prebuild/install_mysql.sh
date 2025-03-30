43836b8bf9e8594e1e9b90746375f004eaf53b6c
Ahamed Abdul Khader <ahamedabdul24@gmail.com>
2025-03-30 20:50:28 +0100

    code change

--- .platform/hooks/prebuild/install_mysql.sh
+++ .platform/hooks/prebuild/install_mysql.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-# Update package lists
-yum update -y
+# Update package information
+sudo dnf update -y
 
-# Install MySQL development libraries compatible with Amazon Linux 2
-yum install -y mysql mysql-server mysql-devel
-
-# Alternatively, try installing MariaDB-devel if mysql-devel is not found
-yum install -y mariadb mariadb-server mariadb-devel
+# Install MySQL or MariaDB server and development libraries
+sudo dnf install -y mysql mysql-server mysql-devel || sudo dnf install -y mariadb mariadb-server mariadb-devel
