1e120fef593ec5bb1622a2658e296d7de1a6170e
Ahamed Abdul Khader <ahamedabdul24@gmail.com>
2025-03-30 21:27:53 +0100

    code change

--- .platform/hooks/prebuild/install_mysql.sh
+++ .platform/hooks/prebuild/install_mysql.sh
@@ -2,10 +2,26 @@
 # install_mysql.sh
 
 # Update the package repository
-sudo dnf update -y
+# sudo dnf update -y
 
-# Install MySQL 8.0
-sudo dnf install -y mysql mysql-server mysql-devel
+# # Install MySQL 8.0
+# sudo dnf install -y mysql mysql-server mysql-devel
 
 # If you want MariaDB, use the following command instead:
 # sudo dnf install -y mariadb mariadb-devel
+
+
+#!/bin/bash
+
+# Update package repository
+sudo yum update -y
+
+# Install MariaDB server and development libraries
+sudo yum install -y mariadb-server mariadb-devel
+
+# Start the MariaDB service
+sudo systemctl start mariadb
+sudo systemctl enable mariadb
+
+# Secure the MariaDB installation (optional but recommended)
+sudo mysql_secure_installation
