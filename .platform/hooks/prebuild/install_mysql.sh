cfdae1da9aa9c80cc682e5c951275ee00dc5fc3b
Ahamed Abdul Khader <ahamedabdul24@gmail.com>
2025-03-30 21:19:45 +0100

    code change

--- .platform/hooks/prebuild/install_mysql.sh
+++ .platform/hooks/prebuild/install_mysql.sh
@@ -2,13 +2,10 @@
 # install_mysql.sh
 
 # Update the package repository
-sudo yum update -y
+sudo dnf update -y
 
-# Install MySQL 8.0 using amazon-linux-extras (for Amazon Linux 2)
-sudo amazon-linux-extras install -y mysql8.0
+# Install MySQL 8.0
+sudo dnf install -y mysql mysql-server mysql-devel
 
-# Install MySQL client and development libraries
-sudo yum install -y mysql-devel
-
-# Alternatively, if you prefer MariaDB instead of MySQL, you can use the following:
-# sudo yum install -y mariadb mariadb-devel
+# If you want MariaDB, use the following command instead:
+# sudo dnf install -y mariadb mariadb-devel
